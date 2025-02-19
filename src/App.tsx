eb8ed47e42a9b470c1e08939a2ac1ab4ed7437c2
Daniel Morton <daniel@djmorton.com>
2025-02-18 21:07:54 -0500

    Fix text entry field not being cleared appropriatly

--- src/App.tsx
+++ src/App.tsx
@@ -37,6 +37,7 @@ function App() {
 
   const handleClear = () => {
     setRollResult(null)
+    setFormula('')
   }
 
   const handleReroll = (formula: string) => {
@@ -80,9 +81,10 @@ function App() {
   return (
     <div className="min-h-screen bg-slate-800 text-white">
       <div className="p-8">
-        <header className="text-center mb-8">
+        <header className="text-center mb-8 text-sm">
           <h1 className="text-4xl font-bold mb-2">RPG Dice Roller</h1>
-          <p className="text-slate-300">Roll any combination of dice with ease</p>
+          <p className="text-slate-300">Enter a dice formula (ie. 5d6 + 1d8 + 3).<br />Prefix with &lt; to take the least of the group (&lt;2d20).<br />
+           Prefix with &gt; to take the greatest of the group (&gt;2d20).</p>
         </header>
         
         <div className="flex justify-center">
